12bd50e80ad7afe7072ce31d6e42da69aca037c3
Félix Mielcarek <felix.mielcarek10@gmail.com>
2025-06-18 10:40:46 +0200

    Remove Firebase

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 //#region IMPORTS
-import express from 'express';
-import axios from 'axios';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import admin from 'firebase-admin';
+import express from 'express';  // to start web server
+import axios from 'axios';  // to execute HTTP request
+import cors from 'cors';  // to allow external interaction
+import dotenv from 'dotenv';  // for environment variables
 //#endregion
 
 //#region CONFIGURATIONS
 dotenv.config();
-
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  })
-});
-const db = admin.firestore();
 
 const clientId = process.env.SPOTIFY_CLIENT_ID;
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 const port = process.env.SERVER_PORT;
 const redirectUri = process.env.SPOTIFY_REDIRECT_URL;
 const allowedDomain = [ process.env.ALLOWED_DOMAIN ];
+const databaseUrl = process.env.DATABASE_URL;
+const frontendUrl = process.env.FRONTEND_URL;
 //#endregion
 
 //#region LOGS
@@ -36,7 +28,7 @@ function stepBeggining(step) {
 const app = express()
 app.use(cors({ origin: allowedDomain }));
 app.use(express.json());
-app.listen(port, () => { console.log(`Big brother is listening on port ${port}`) })
+app.listen(port, () => { console.log(`Big brother server is listening on port ${port}`) })
 //#endregion
 
 //#region ACCESS TOKEN
@@ -79,19 +71,23 @@ app.get('/callback', async (req, res) => {
     };
   } catch (error) {
     console.log(`Error getting user Spotify id: ${error}`);
+    console.log(error);
     res.status(500).send('Error');
   }
 
   try {
-    await db.collection("users").doc(data.SpotifyId).set({
+    await axios.post(`${databaseUrl}/users`, {
+      spotifyId: data.SpotifyId,
       accessToken: data.AccessToken,
-      refreshToken: data.RefreshToken,
-    });
-    
+      refreshToken: data.RefreshToken
+    })
+
     console.log('Data inserted/updated successfully');
-    res.status(200).send('Ok');
+    //res.status(200).send('Ok');
+    res.redirect(`${frontendUrl}`);
   } catch (error) { 
     console.log(`Error accessing database: ${error}`);
+    console.log(error);
     res.status(500).send('Error');
   }
 });
